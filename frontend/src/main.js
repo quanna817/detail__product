04a1808e602e5abb0f84378e1309f3db8ff4f6b9
QuanNA <naquan@netvisiontel.com>
2024-04-15 06:23:14 +0000

    add price_history of product

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,7 +7,7 @@ import router from './router';
 const app = createApp(App);
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:8000/'; 
+axios.defaults.baseURL = 'http://127.0.0.1:8000/'; 
 
 app.use(router);
 app.mount("#app");
